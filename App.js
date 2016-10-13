b1cda0ae69e4b8ebe495948a52eb0c6edc79d239
munens <kaumbuthom@gmail.com>
2016-10-12 22:29:49 -0700

    vid 3: every return statement of a render() method must be enclosed within a single tag

--- App.js
+++ App.js
@@ -5,9 +5,20 @@ import React from 'react';
 class App extends React.Component {
     render(){
         // inside the return we write our jsx:
-        return (<h1>Hello People</h1>);
-        // every element made here is given to the React.createElement(), method that takes 3 elements: 
+        // vid 1 - 2:
+        //return (<h1>Hello People</h1>);
+        // 1. every element made here is given to the React.createElement(), method that takes 3 elements: 
         // the html element, the props ("") and the inner html eg. React.createElement('h1', null, 'hello guys')
+        // 2. everything in the return must be enclosed in a single tag eg. <div> , otherwise it cannot be rendered.
+        // It would mean returning 2 React.createElements and this will note make sense.
+        // therefore:
+        // vid 3:
+        return (
+            <div>
+                <h1>Hello People</h1>
+                <b>Bold font</b>
+            </div>
+        );
     }
 }
 
