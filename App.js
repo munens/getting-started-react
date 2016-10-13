c542b68b9d338f60aa7ac661489836f39dd14874
munens <kaumbuthom@gmail.com>
2016-10-12 22:22:08 -0700

    vid2: difference state vs stateless components

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
-//this class below is essentally an object, whose render() method renders the html that appears on the page.
+//method 1: A class has state
+//this class below is a component, whose render() method renders the html that appears on that page for that component.
 class App extends React.Component {
     render(){
-        return (<div>Hello</div>);
+        // inside the return we write our jsx:
+        return (<h1>Hello People</h1>);
+        // every element made here is given to the React.createElement(), method that takes 3 elements: 
+        // the html element, the props ("") and the inner html eg. React.createElement('h1', null, 'hello guys')
     }
 }
+
+//method 2: a stateless component, can be created by:
+//const = App = () => <h1>Hello People</h1>
+
 
 export default App;
