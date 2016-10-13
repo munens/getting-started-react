f9d8e1c24c90383a6dd532f98dc81ed391b13d66
munens <kaumbuthom@gmail.com>
2016-10-12 23:16:54 -0700

    how to add props

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
+/* vid 1 -3:
 import React from 'react';
 
 //method 1: A class has state
 //this class below is a component, whose render() method renders the html that appears on that page for that component.
+// the name is capitalised to show it is a component.
 class App extends React.Component {
+
     render(){
         // inside the return we write our jsx:
         // vid 1 - 2:
@@ -19,7 +22,7 @@ class App extends React.Component {
                 <b>Bold font</b>
             </div>
         );
-    }
+    };
 }
 
 //method 2: a stateless component, can be created by:
@@ -27,3 +30,49 @@ class App extends React.Component {
 
 
 export default App;
+
+*/
+
+/* vid 4: */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render(){
+        let txt = this.props.txt;
+        return (<h1>Hello People: {txt}</h1>);
+    };
+}
+
+
+
+//add a propType: we can define what form our props should be in for example, string, number etc.
+App.propType = {
+    txt: React.PropTypes.string,
+
+    // and they can be made into a required property:
+    cat: React.PropTypes.number.isRequired
+}
+
+// add a default property: we can set default props for the component. Such that if we were to remove the props, 
+// that we want to eventually set hen the default one would show up and vice versa.
+App.defaultProps = {
+    txt: "this is the default txt"
+}
+
+// The ReactDOM.render(), may appear on any component. 
+// Here we are going to show how we can pass props onto a React component - whixh is i.e. giving the component a value.
+// When this is done the component recieves the value as an object in terms of this.props - that can now be used by the
+// component, as we can see.
+ReactDOM.render(
+    <App  cat={5} />, 
+    document.getElementById('app') 
+);
+
+export default App;
+
+
+
+
+
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* vid1 
 import React from 'react';
 import ReactDom from 'react-dom';
 
@@ -5,3 +6,7 @@ import App from './App';
 
 // render the page in which the entire application will be rendered.
 ReactDom.render( <App/>, document.getElementById('app'));
+*/
+
+/*vid 4: */
+import App from './App';
